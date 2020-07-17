c5a99eac2b17c5c5b83fe671381f80e5d91d684d
sidyadav2211 <kailashyadav1998@example.com>
2020-07-17 14:40:50 +0530

    added

---
+++ app.py
@@ -0,0 +1,16 @@
+# Buy your laptop by your requirement need
+
+requirement = input('Enter your laptop Use ex personal & coding etc :')
+
+if requirement.upper() == 'Personal'.upper():
+
+    print('Personal')
+
+elif requirement.upper() == 'Coding'.upper():
+    print('Coding')
+elif requirement.upper() == 'Gaming'.upper():
+    print('Gaming')
+elif requirement.upper() == 'Editing'.upper():
+    print('Editing laptop')
+else:
+    print('Not found')
